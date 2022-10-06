fef650a6d654d5e817a9eb7f56facc9c864e68c0
Ali Farahmand <74143303+farahmandaliir@users.noreply.github.com>
2022-10-06 19:53:09 +0330

    start to create intro section
    add font
    add intro section
    add nav item
    add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    }
   },
   plugins: [],
 }
